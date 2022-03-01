f112332c51cf2e79bc2eddc27e1f2abb0b9b1ee7
Nicolassantos777 <90699348+Nicolassantos777@users.noreply.github.com>
2022-03-01 13:03:48 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+function copy1(){
+  document.querySelector("#inp1").select();
+  document.execCommand("copy");
+}
+
+function copy2(){
+  document.querySelector("#inp2").select();
+  document.execCommand("copy");
+}
+
+function copy3(){
+  document.querySelector("#inp3").select();
+  document.execCommand("copy");
+}
